174dbda6c04e220074b033d71ee7107184f632b1
o0beaner <o0beaner@gmail.com>
2018-02-23 16:13:13 -0600

    switched to nice kill

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 
 forever_pid=$(ps aux | grep [f]orever | tr -s " " | cut -d " " -f 2)
-kill -9 $forever_pid
+kill $forever_pid
 node_pid=$(ps aux | grep [n]ode | tr -s " " | cut -d " " -f 2)
-kill -9 $node_pid
+kill $node_pid
